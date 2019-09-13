7060599ec94c46842556fceb16bfc6c55c562d1f
menaka-siri <menaka.12@cse.mrt.ac.lk>
2019-09-13 12:28:06 +0530

    added product-detail page route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailComponent}
     ])
   ],
   declarations: [
